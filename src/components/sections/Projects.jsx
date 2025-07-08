586c9abb9faeb659808876330f6f4587e5d39b5d
GuutaaH <63647222+GuutaaH@users.noreply.github.com>
2025-07-08 01:17:47 -0400

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -43,6 +43,35 @@ export const Projects = () => {
                         </div>
                         {/* End of Project */}
 
+                        
+                          {/* Start of Project */}
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2">Autoglow</h3>
+                            <p className="text-gray-400 mb-4">
+                                Developed a website for a local care detailing buissness
+                            </p>
+
+                            {/* Technologies */}
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["Tailwindcss", "React"].map((tech, key) => (
+                                    <span 
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                                    > 
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+
+                            {/* Project Link */}
+                            <div className="flex justify-between items-center">
+                                <a href="https://autoglow-detailing.vercel.app/" className="text-blue-400 hover:text-blue-300 transition-colors my-4" target="_blank"> 
+                                    View Project
+                                </a>
+                            </div>
+                        </div>
+                        {/* End of Project */}
+
                     </div>
                 </div>
             </RevealOnScroll>
