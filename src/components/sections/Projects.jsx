d265856e70cfefe8c218c84b29f3d1716810e9d2
Real <guutaahassen2@gmail.com>
2025-07-08 21:08:09 -0400

    local update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -6,23 +6,17 @@ export const Projects = () => {
         <section id="projects" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
                 <div className="max-w-5xl mx-auto px-4 flex flex-col items-center">
-                    
-                    {/* Section Title */}
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"> 
                         Featured Projects
                     </h2>
 
-                    {/* Projects Grid (Centered) */}
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center">
-                        
-                        {/* Start of Project */}
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                        {/* AI Chatbot */}
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                             <h3 className="text-xl font-bold mb-2">AI Chatbot</h3>
                             <p className="text-gray-400 mb-4">
                                 Developed an interactive AI chatbot that responds to users in real-time using the Amazon Bedrock, AWS Amplify and React.
                             </p>
-
-                            {/* Technologies */}
                             <div className="flex flex-wrap gap-2 mb-4">
                                 {["React", "AWS Amplify"].map((tech, key) => (
                                     <span 
@@ -33,16 +27,45 @@ export const Projects = () => {
                                     </span>
                                 ))}
                             </div>
-
-                            {/* Project Link */}
                             <div className="flex justify-between items-center">
-                                <a href="https://master.d1udfopqcaa00y.amplifyapp.com/" className="text-blue-400 hover:text-blue-300 transition-colors my-4" target="_blank"> 
+                                <a 
+                                    href="https://master.d1udfopqcaa00y.amplifyapp.com/" 
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4" 
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
                                     View Project
                                 </a>
                             </div>
                         </div>
-                        {/* End of Project */}
 
+                        {/* Autoglow */}
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2">Autoglow</h3>
+                            <p className="text-gray-400 mb-4">
+                                Developed a website for a local car detailing business.
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["Tailwind CSS", "React"].map((tech, key) => (
+                                    <span 
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                                    > 
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <a 
+                                    href="https://autoglow-detailing.vercel.app/" 
+                                    className="text-blue-400 hover:text-blue-300 transition-colors my-4" 
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    View Project
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </RevealOnScroll>
